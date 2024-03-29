14472605990e15ac54271e5a519a7b54d6eef23c
Muneeb Aamir <89025139+MuneebAamir1@users.noreply.github.com>
2024-03-29 23:07:45 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+import inquirer from "inquirer";
+async function calculate() {
+    let answers = await inquirer.prompt([
+        {
+            name: "firstNumber",
+            message: "Enter first number",
+            type: "number",
+        },
+        {
+            name: "secondNumber",
+            message: "Enter second number",
+            type: "number",
+        },
+        {
+            name: "operator",
+            message: "Select Operator",
+            type: "list",
+            choices: ["Add", "Subtract", "Multiply", "Divide"],
+        },
+    ]);
+    console.log(answers);
+    let result;
+    if (answers.operator === "Add") {
+        // Addition
+        result = answers.firstNumber + answers.secondNumber;
+    }
+    else if (answers.operator === "Subtract") {
+        // Subtraction
+        result = answers.firstNumber - answers.secondNumber;
+    }
+    else if (answers.operator === "Multiply") {
+        // Multiplication
+        result = answers.firstNumber * answers.secondNumber;
+    }
+    else if (answers.operator === "Divide") {
+        // Division
+        result = answers.firstNumber / answers.secondNumber;
+    }
+    console.log(`Result is ${result}`);
+}
+calculate();
